255a022ab03baa27d4661510e9cf21fad2970eb6
Fran López <62934773+dixho@users.noreply.github.com>
2022-01-24 15:34:42 +0100

    1

--- proyectos/Calculadora/js/main.js
+++ proyectos/Calculadora/js/main.js
@@ -1,13 +1,16 @@
 window.addEventListener("load", inicio, false)
 
+var x
+var aa
 function inicio(){
     console.log("Cargado")
-    document.getElementById("boton").addEventListener("click", boton1, false)
+    document.getElementById("boton44").addEventListener("click", boton1,false)
 }
 
-function boton1(event){
+function boton1(evnt){
     console.log("ff");
-    let x = event.target;
-    console.log(x)
+     x = evnt.target;
+     aa = x.value
+    console.log(x," - ",aa)
 
 }
