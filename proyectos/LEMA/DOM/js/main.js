8176fc8faff6c8c6c905b90d7da8f3315cbbe414
Fran López <62934773+dixho@users.noreply.github.com>
2022-01-28 15:45:32 +0100

    1

---
+++ proyectos/LEMA/DOM/js/main.js
@@ -0,0 +1,11 @@
+// const body = document.body
+// const div = document.createElement("div")
+
+// // div.innerText = "<h1>Hello World</h1>"
+// div.textContent = "Hello World 2"
+// body.append(div)
+
+const div = document.querySelector("div")
+
+console.log(div.textContent)
+console.log(div.innerText)
